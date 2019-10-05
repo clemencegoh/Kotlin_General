314ba560d620620ff8fb4f78a8b039b26eb7fe82
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-10-05 21:31:25 +0800

    initial commit for new repo

---
+++ src/experiments/HelloWorld.kt
@@ -0,0 +1,16 @@
+package experiments
+
+import java.util.*
+
+
+fun NavigateList(){
+    var ll = LinkedList<Int>()
+    ll.add(2)
+    print(ll.first)
+}
+
+
+fun main() {
+    println("Hello world from experiments!")
+    NavigateList()
+}
